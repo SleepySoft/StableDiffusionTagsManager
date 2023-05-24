348f7fd95f50d8f63b6894ce573c73f34fec3ddf
Sleepy <sleepysoft@gmail.com>
2023-05-24 22:51:26 +0800

    Debug analysis_tag

--- TagManager.py
+++ TagManager.py
@@ -4,7 +4,6 @@ from collections import OrderedDict
 
 from app_utility import *
 
-
 PRIMARY_KEY = 'tag'
 
 DATABASE_SUPPORT_FIELD = OrderedDict([
@@ -20,6 +19,86 @@ DATABASE_SUPPORT_FIELD = OrderedDict([
 ])
 
 DATABASE_FIELDS = list(DATABASE_SUPPORT_FIELD.keys())
+
+
+def try_float(text: str) -> float or None:
+    try:
+        return float(text)
+    except ValueError:
+        return None
+    finally:
+        pass
+
+
+def parse_wrapper(tag: str, left: str, right: str, base: float) -> (str, float):
+    tag = tag.strip()
+    if tag.startswith(left):  # and tag.endswith(right):
+        n = tag.count(left)
+        weight = base ** n
+    else:
+        weight = 1.0
+    return tag.replace(left, '').replace(right, ''), weight
+
+
+def analysis_tag(tag: str):
+    weight_list = []
+    tag_list = []
+    if tag.startswith('['):
+        raw_tag, weight = parse_wrapper(tag, '[', ']', 0.9)
+        tag_list.append(raw_tag)
+        weight_list.append(weight)
+    elif tag.startswith('{'):
+        raw_tag, weight = parse_wrapper(tag, '{', '}', 1.1)
+        tag_list.append(raw_tag)
+        weight_list.append(weight)
+    elif tag.startswith('('):
+        if ':' in tag:
+            sub_tags = tag.replace('(', '').replace(')', '').split(':')
+            for sub_tag in sub_tags:
+                weight = try_float(sub_tag)
+                if weight:
+                    weight_list.append(weight)
+                else:
+                    tag_list.append(sub_tag)
+        else:
+            raw_tag, weight = parse_wrapper(tag, '(', ')', 1.1)
+            tag_list.append(raw_tag)
+            weight_list.append(weight)
+    elif tag.startswith('<'):
+        sub_tags = tag.replace('<', '').replace('>', '').split(':')
+
+        weight = try_float(sub_tags[2]) if len(sub_tags) > 2 else None
+        weight = 1.0 if weight is None else weight
+        raw_tag = sub_tags[0] + ':' + sub_tags[1] if len(sub_tags) > 1 else sub_tags[0]
+
+        tag_list.append(raw_tag)
+        weight_list.append(weight)
+    else:
+        tag_list.append(tag)
+        weight_list.append(1.0)
+
+    return tag_list, weight_list
+
+
+print(analysis_tag("<lora:add_detail:1.5>"))
+print(analysis_tag("<lora:add_detail>"))
+print(analysis_tag("<lora"))
+
+print(analysis_tag("tag"))
+print(analysis_tag("(tag:1.2)"))
+print(analysis_tag("(tag1:tag2:1.1:1.2)"))
+
+print(analysis_tag("(abc)"))
+print(analysis_tag("((abc))"))
+print(analysis_tag("(((abc)))"))
+
+print(analysis_tag("{def}"))
+print(analysis_tag("{{def}}"))
+print(analysis_tag("{{{def}}}"))
+
+print(analysis_tag("[xyz]"))
+print(analysis_tag("[[xyz]]"))
+print(analysis_tag("[[[xyz]]]"))
 
 
 class TagManager:
@@ -128,6 +207,10 @@ class TagManager:
 
     @staticmethod
     def parse_prompts(prompt_text: str):
+        return TagManager.parse_prompts_2(prompt_text)
+
+    @staticmethod
+    def parse_prompts_1(prompt_text: str):
         # Split the prompt_text by '\n' and strip each line, remove empty lines
         lines = [line.strip() for line in prompt_text.split('\n') if line.strip()]
 
@@ -147,6 +230,46 @@ class TagManager:
 
         # Return positive_tags, negative_tags, extra_data
         return positive_tags, negative_tags, extra_data
+
+    @staticmethod
+    def parse_prompts_2(prompt_text: str):
+        prompt_text = prompt_text.strip()
+        lines = prompt_text.split('\n')
+        lines = [line for line in lines if line.strip() != '']
+        prompt_text = '\n'.join(lines)
+
+        positive_start = prompt_text.lower().find('positive prompt')
+        if positive_start != -1:
+            positive_start = prompt_text.find(':', positive_start) + 1
+        else:
+            positive_start = 0
+
+        negative_start = prompt_text.lower().find('negative prompt')
+        if negative_start != -1:
+            positive_end = negative_start
+            negative_start = prompt_text.find(':', negative_start) + 1
+            negative_end = prompt_text.find('\n', negative_start)
+        else:
+            first_line_end = prompt_text.find('\n')
+            positive_end = first_line_end if first_line_end > 0 else len(prompt_text)
+            negative_start = positive_end
+            negative_end = positive_end
+
+        positive_tags_string = prompt_text[positive_start:positive_end].strip()
+        negative_tags_string = prompt_text[negative_start:negative_end].strip()
+        extra_data_string = prompt_text[negative_end + 1:].strip()
+
+        # print('Positive tags:', positive_tags_string)
+        # print('Negative tags:', negative_tags_string)
+        # print('Extra data:', extra_data_string)
+
+        positive_tags_raw = positive_tags_string.replace('\n', ',').split(',')
+        negative_tags_raw = negative_tags_string.replace('\n', ',').split(',')
+
+        positive_tags = [tag.strip() for tag in positive_tags_raw if tag.strip() != '']
+        negative_tags = [tag.strip() for tag in negative_tags_raw if tag.strip() != '']
+
+        return positive_tags, negative_tags, extra_data_string
 
     @staticmethod
     def analysis_tag(tag: str) -> (str, str):
@@ -297,3 +420,17 @@ class TagManager:
                 current_item = child_item
 
         tree_widget.expandAll()
+
+
+class Prompts:
+    def __init__(self):
+        self.positive_tags = pd.DataFrame(columns=DATABASE_FIELDS)
+        self.negative_tags = pd.DataFrame(columns=DATABASE_FIELDS)
+        self.extra_data = ''
+
+    def from_text(self, text: str) -> bool:
+        self.positive_tags, self.negative_tags, self.extra_data = TagManager.parse_prompts(text)
+
+    def from_file(self, file_name: str) -> bool:
+        with open(file_name, 'rt') as f:
+            return self.from_text(f.read())
