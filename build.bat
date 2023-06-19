95db7fc46842028c57efb4cf397be76ea90adfe6
Sleepy <sleepysoft@gmail.com>
2023-06-19 14:30:16 +0800

    Update build script and update tags

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 python -m venv pack
 call pack\Scripts\activate
-pip install -r Requirements.txt
+pip install -r requirements.txt
 pyinstaller.exe -w -i doc/s.ico --add-data "public.csv;." --add-data "depot;depot" --add-data "README.md;." main.py
